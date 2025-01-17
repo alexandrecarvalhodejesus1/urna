3349eecd88006141c8b67b1db6bbfb4b63ff44a8
alexandrecarvalhodejesus1 <alexandre.jesus2@stellantis.com>
2025-01-17 14:07:13 -0300

    opa

--- src/app/botoes/botoes.component.ts
+++ src/app/botoes/botoes.component.ts
@@ -10,10 +10,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BotoesComponent {
 
-  @Input() numeros7a9 = false;
-  @Input() numeros4a6 = false;
-  @Input() numeros1a3 = false;
-  @Input() numero0 = false;
+
 
   
 }
