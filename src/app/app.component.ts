f72b53177876865cc8b424603cab445b43c9a147
alexandrecarvalhodejesus1 <alexandre.jesus2@stellantis.com>
2025-01-29 09:44:04 -0300

    Implementação visualização números tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
 import { BotoesComponent } from './botoes/botoes.component';
+import { NumeroTelaComponent } from "./numero-tela/numero-tela.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [BotoesComponent]
+  imports: [BotoesComponent, NumeroTelaComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+  numeroDigitado1: string = '';
+  numeroDigitado2: string = '';
+  numeroDigitado3: string = '';
+
+  CapturarNumeroClicado(numeroClicado: number) {
+    
+    if (this.numeroDigitado1.length == 0) {
+      this.numeroDigitado1 = numeroClicado.toString();
+      return;
+    }
+
+    if (this.numeroDigitado2.length == 0) {
+
+      this.numeroDigitado2 = numeroClicado.toString();
+      return;
+    }
+
+    if (this.numeroDigitado3.length == 0) {
+      this.numeroDigitado3 = numeroClicado.toString();
+    }
+  }
+
+}
+
+
--- src/app/botoes/botoes.component.ts
+++ src/app/botoes/botoes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,10 +9,15 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./botoes.component.scss']
 })
 export class BotoesComponent {
-  numbers: number[][] = [
-    [9, 8, 7],
-    [6, 5, 4],
-    [3, 2, 1],
-    [0]
-  ];
+  numbers: number[] = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+  @Output() numeroClicado = new EventEmitter<number>();
+
+  //Método é composto por um nome seguido de parenteses que pode conter ou não PARAMETROS, e em seguida contém as chaves {} que tem a implementação de código
+  ObterNumeroClicado(numero: number)
+  {
+    //implementação
+    this.numeroClicado.emit(numero);
+  }
+
 }
